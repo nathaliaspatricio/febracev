529fc23ad407a7e2dda21a1f26041f580630fc27
Leandro Coletto Biazon <leandrobiazon@gmail.com>
2009-05-27 18:16:22 -0300

    Apenas categorias válidas geram páginas de projetos

--- projects/views.py
+++ projects/views.py
@@ -2,11 +2,12 @@
 from django.views.generic import list_detail
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
+from django.http import Http404
 
-from projects.models import Project
+from projects.models import Project, CATEGORIES
 
 def projects_by_year(request, year):
-   return list_detail.object_list(
+    return list_detail.object_list(
                request,
                queryset = Project.objects.filter(edition=year),
                template_object_name = 'project',
@@ -14,12 +15,15 @@ def projects_by_year(request, year):
            )
 
 def projects_by_category(request, category):
-   return list_detail.object_list(
-               request,
-               queryset = Project.objects.filter(category=category),
-               template_object_name = 'project',
-               extra_context = {'title': category}
-           )
+    if (category not in [cat[0] for cat in CATEGORIES]):
+        raise Http404
+    else:
+        return list_detail.object_list(
+                   request,
+                   queryset = Project.objects.filter(category=category),
+                   template_object_name = 'project',
+                   extra_context = {'title': category}
+               )
 
 def project_detail(request, year, category, code):
     project = get_object_or_404(Project, edition=year, category=category, code=code)
