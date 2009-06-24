9377a6a7fb7f0d0485d6c2b9a7b3f94b9524345e
Leandro Coletto Biazon <leandrobiazon@gmail.com>
2009-06-24 16:37:06 -0300

    remoção do arquivo monografia.pdf para evitar conflitos do git

--- projects/utils.py
+++ projects/utils.py
@@ -1,6 +1,6 @@
 from projects.models import Project, ProjectLink
 from django.contrib.auth.models import User
- 
+
 def get_favorite_projects(user):
     pl = ProjectLink.objects.filter(user=user).values('project')
     return Project.objects.filter(id__in=[i['project'] for i in pl])
