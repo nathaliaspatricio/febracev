6f2597c02c730a7275122e3ff609061a6452f8ae
Leandro Coletto Biazon <leandrobiazon@gmail.com>
2009-07-02 23:12:00 -0300

    Busca retorna colunas

--- search/views.py
+++ search/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.decorators import login_required
 from projects.models import Project
 from django.contrib.auth.models import User
 from tagging.models import Tag
+from articles.models import Article
 
 from search.utils import parse_search_input
 
@@ -14,17 +15,20 @@ def search(request, search_input):
 
     projects = Project.objects.none()
     users = User.objects.none()
+    articles = Article.objects.none()
     tags = Tag.objects.none()
 
     for term in search_terms:
-        projects = projects & Project.objects.filter(name__contains=term)
-        users = users & User.objects.filter(username__contains=term)
-        tags = tags & Tag.objects.filter(name__contains=term)
+        projects = projects | Project.objects.filter(name__contains=term)
+        users = users | User.objects.filter(username__contains=term)
+        articles = articles | Article.objects.filter(title__contains=term)
+        tags = tags | Tag.objects.filter(name__contains=term)
 
     return render_to_response('search/search_results.html',
                               {'title': unicode(search_input),
                                'project_list': projects,
                                'user_list': users,
+                               'article_list': articles,
                                'tag_list': tags,
                               },
                               context_instance = RequestContext(request))
