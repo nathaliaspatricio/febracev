4c3dd9a79fcc1e9fe77e9210f3cad74ec58ea8b0
Nathalia Sautchuk Patrício <nathalia.sautchuk@gmail.com>
2009-07-02 23:42:59 -0300

    dinamizando os artigos na home

--- home/views.py
+++ home/views.py
@@ -2,13 +2,12 @@ from django.views.generic import list_detail
 from django.shortcuts import get_object_or_404, render_to_response, redirect
 from django.template import RequestContext
 
-from projects.models import Project, ProjectLink, CATEGORIES
+from articles.models import Article
 
 def index(request):
-    years = range(2003,2010)
-    categories = list(CATEGORIES)
+    article_list = Article.objects.all()
     return render_to_response('home/index.html',
-                              {'years': years,
-                               'categories': categories},
+                              {'article_list': article_list,
+                              },
                               context_instance = RequestContext(request))
 
